01c61e71125956d2a3bc1b8c2b2d7775a2cd2ae0
Žan Ožbot <zan.ozbot@gmail.com>
2020-08-07 17:27:33 +0200

    Implemented button web component

--- src/components/mwcpl-button/mwcpl-button.tsx
+++ src/components/mwcpl-button/mwcpl-button.tsx
@@ -2,7 +2,7 @@ import { Component, ComponentInterface, h, Prop } from '@stencil/core';
 
 @Component({
   tag: 'mwcpl-button',
-  styleUrl: 'mwcpl-button.css',
+  styleUrl: 'mwcpl-button.scss',
   shadow: true,
 })
 export class MwcplButton implements ComponentInterface {
@@ -12,9 +12,46 @@ export class MwcplButton implements ComponentInterface {
    */
   @Prop() label: string;
 
+  /**
+   * Is the button text uppercase
+   */
+  @Prop() uppercase: boolean;
+
+  /**
+   * Is the button fullwidth
+   */
+  @Prop() fullwidth: boolean;
+
+  /**
+   * Is the button outlined
+   */
+  @Prop() outlined: boolean;
+
+  /**
+   * Is the button disabled
+   */
+  @Prop() disabled: boolean;
+
+  /**
+   * Is the button rounded
+   */
+  @Prop() rounded: boolean;
+
+  /**
+   * Is the button loading
+   */
+  @Prop() loading: boolean;
+
   render() {
     return (
-      <button type="button">{this.label}</button>
+      <button type="button" disabled={this.disabled}>
+        <span class="loading">
+          <span class="line"></span>
+        </span>
+        <slot name="leading-icon"></slot>
+        <span>{this.label}</span>
+        <slot name="trailing-icon"></slot>
+      </button>
     );
   }
 
